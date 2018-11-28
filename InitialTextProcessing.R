c4b54e25da41b34c7516d1348ec206ecdf6a51f8
modulus100 <aleksandr.tserepannikov@tptlive.ee>
2018-11-28 15:32:54 +0200

    init processing

---
+++ InitialTextProcessing.R
@@ -0,0 +1,38 @@
+initialTextProcessing <- function() {
+  rm(list = ls())
+  cname <- file.path("/Users/aleksandrmadisson/Repository/DataMining/TextMiningAnalysis/samples")
+  dir(cname)
+  
+  # load package tm  which is framework for text data mining
+  library(tm)
+  
+  # load available documents
+  docs <- VCorpus(DirSource(cname))   
+  print(summary(docs))
+  
+  # Preprocessing
+  docs <- tm_map(docs,removePunctuation)   
+  docs <- tm_map(docs, removeNumbers)   
+  docs <- tm_map(docs, tolower)   
+  docs <- tm_map(docs, removeWords, stopwords("english"))   
+  docs <- tm_map(docs, removeWords, c("syllogism", "tautology"))   
+  docs <- tm_map(docs, stripWhitespace)
+  docs <- tm_map(docs, PlainTextDocument)
+  
+  dtm <- DocumentTermMatrix(docs)   
+  
+  print(dtm)
+  
+  freq <- colSums(as.matrix(dtm))   
+  length(freq)
+  dtms <- removeSparseTerms(dtm, 0.1)
+  print(dtms)
+  
+  # table after removing sparse terms
+  freq <- colSums(as.matrix(dtms))  
+  print(freq)
+  
+  print(findFreqTerms(dtm, lowfreq=30))
+  dtms <- removeSparseTerms(dtm, 0.15)
+  return(dtms)
+}
--- Main.R
+++ Main.R
@@ -1,46 +1,16 @@
-rm(list = ls())
-cname <- file.path("/Users/aleksandrmadisson/Repository/DataMining/TextDatamining2018/samples")
-#cname <- file.path("/Users/aleksandrmadisson/Repository/DataMining/TextDatamining2018/customSamples")
-dir(cname)
+source("InitialTextProcessing.R")
 
-# load package tm  which is framework for text data mining
-library(tm)
+# Get processed data for analysis
+dataSet <- initialTextProcessing()
 
-# load available documents
-docs <- VCorpus(DirSource(cname))   
-print(summary(docs))
-
-# Preprocessing
-docs <- tm_map(docs,removePunctuation)   
-docs <- tm_map(docs, removeNumbers)   
-docs <- tm_map(docs, tolower)   
-docs <- tm_map(docs, removeWords, stopwords("english"))   
-docs <- tm_map(docs, removeWords, c("syllogism", "tautology"))   
-docs <- tm_map(docs, stripWhitespace)
-docs <- tm_map(docs, PlainTextDocument)
+# Cosine distance
 
 
-dtm <- DocumentTermMatrix(docs)   
-
-
-print(dtm)
-
-freq <- colSums(as.matrix(dtm))   
-length(freq)   
-#print(freq) 
-dtms <- removeSparseTerms(dtm, 0.1)
-print(dtms)
-
-# table after removing sparse terms
-freq <- colSums(as.matrix(dtms))  
-print(freq)
-
-print(findFreqTerms(dtm, lowfreq=30)) 
 
 # K-means
 library(fpc)
 library(cluster)
-dtms <- removeSparseTerms(dtm, 0.15)
-d <- dist(t(dtms), method="manhattan")
+d <- dist(t(dataSet), method="manhattan")
 kfit <- kmeans(d, 2)
+#print()
 clusplot(as.matrix(d), kfit$cluster, color=T, shade=T, labels=2, lines=0)
